c4bb516720d04b4a9ae113cb5e9d5b17ca651cd2
eryadavmukesh <eryadavmukesh@gmail.com>
2025-05-13 17:53:17 -0400

    changed UI formating.

--- src/ui.py
+++ src/ui.py
@@ -3,15 +3,31 @@ from utils.thought_generator import ThoughtGenerator
 import json
 
 
-# Page configuration
 st.set_page_config(page_title="Thought of the Day", page_icon="🌟", layout="centered")
+
+st.markdown(
+    """
+    <h1 style='text-align: center; font-family: "Segoe UI", "Roboto", "Helvetica Neue", sans-serif; font-size: 40px;'>
+        Thought <span style='color: goldenrod;'>of the Day</span>
+    </h1>
+""",
+    unsafe_allow_html=True,
+)
 
 # Initialize ThoughtGenerator
 generator = ThoughtGenerator()
 
 # UI Layout
-st.title("🌟 Thought of the Day")
-st.write("Enter a theme (e.g., motivation, peace) or leave blank for a random thought.")
+# Centered text
+st.markdown(
+    """
+<div style='text-align: center; font-family: "Segoe UI", "Roboto", "Helvetica Neue", sans-serif; font-size: 20px; color: #FAF9F6;'>
+            Enter a theme (e.g., motivation, peace) or leave blank for a random thought.
+            </div>
+""",
+    unsafe_allow_html=True,
+)
+
 
 # Input form
 with st.form(key="thought_form"):
@@ -29,12 +45,19 @@ if submit_button:
         else:
             # Display formatted JSON output
             st.subheader("Your Thought of the Day")
-            st.json(result)  # Displays JSON nicely
+            # st.json(result)  # Displays JSON nicely
             # Additional styled display
+            # st.markdown(
+            #     f"""
+            #     **Thought**: {result['thought']}
+            #     **Theme**: {result['theme']}
+            #     """,
+            #     unsafe_allow_html=True,
+            # )
             st.markdown(
                 f"""
-                **Thought**: {result['thought']}  
-                **Theme**: {result['theme']}
+                {result['thought']}  
+                
                 """,
                 unsafe_allow_html=True,
             )
